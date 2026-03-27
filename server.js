6d4572667d9446ab25c05cfe92b5ab12b621f697
Devanshu <dkbhaibusiness@gmail.com>
2026-03-27 15:24:49 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/get-video", async (req, res) => {
+    try {
+        let url = req.query.url;
+
+        const response = await axios.get(url);
+        let html = response.data;
+
+        let match = html.match(/"downloadUrl":"(.*?)"/);
+
+        if (!match) {
+            return res.json({ error: "Video not found" });
+        }
+
+        let videoUrl = match[1].replace(/\\u002F/g, "/");
+
+        res.json({ video: videoUrl });
+
+    } catch {
+        res.json({ error: "Error" });
+    }
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Running"));
